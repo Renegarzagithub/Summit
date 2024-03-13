18394d45c19b95dbe5221c16bb612524976f9a36
Rene <rene.garza@red-gate.com>
2024-03-13 12:22:02 -0400

    Added Customer Column

--- schema-model/Tables/dbo.CustomerDemographics.sql
+++ schema-model/Tables/dbo.CustomerDemographics.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[CustomerDemographics]
 (
 [CustomerTypeID] [nchar] (10) NOT NULL,
 [CustomerDesc] [ntext] NULL,
-[nationality] [nvarchar] (20) NULL
+[nationality] [nvarchar] (20) NULL,
+[Customer_Profile] [nchar] (10) NULL
 )
 GO
 ALTER TABLE [dbo].[CustomerDemographics] ADD CONSTRAINT [PK_CustomerDemographics] PRIMARY KEY NONCLUSTERED ([CustomerTypeID])
